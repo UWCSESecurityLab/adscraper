ef342a56a492df4c795f13a5cdec4439f8f62893
Sriram Viswanathan <sriram.vish@gmail.com>
2022-11-15 19:55:30 -0500

    Add more sites to scrape ads on

--- crawler/src/pplt_crawler.ts
+++ crawler/src/pplt_crawler.ts
@@ -18,30 +18,67 @@ let postgres = new Client({
 await postgres.connect();
 
 const sites = [
+    'https://www.huffpost.com/',
+    'https://techcrunch.com/',
     'https://www.nytimes.com',
+    'https://www.foxnews.com/',
     'https://www.washingtonpost.com',
     'https://yahoo.com',
-    'https://www.usatoday.com'
+    'https://www.usatoday.com',
+    'https://www.cbsnews.com/'
 ];
+
+// create a new job
+
+const options = {
+    max_page_crawl_depth: 1,
+    inputs: sites,
+    warm: false,
+    shuffle: true,
+    crawler_hostname: 'localhost',
+    geolocation: '',
+    vpn_hostname: undefined,
+    job_name: `job @ ${new Date().toISOString()}`
+}
+
+
+const queryResult = await postgres.query(
+    `INSERT INTO job (timestamp, max_page_depth, max_depth, input_files,
+      warmed, shuffled, crawler_hostname, geolocation,
+      vpn_hostname, name)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id;`,
+    [
+        new Date(),
+        options.max_page_crawl_depth,
+        options.max_page_crawl_depth * 2,
+        options.inputs.join(', '),
+        options.warm,
+        options.shuffle,
+        options.crawler_hostname,
+        options.geolocation,
+        options.vpn_hostname,
+        options.job_name ? options.job_name : null
+    ]);
+const jobId = queryResult.rows[0].id as number;
 
 for (let site of sites) {
     try {
         await crawler.crawl({
             clearCookiesBeforeCT: false,
             crawlArticle: true,
-            crawlerHostname: 'localhost',
-            crawlPageWithAds: false,
+            crawlerHostname: options.crawler_hostname,
+            crawlPageWithAds: true,
             dataset: 'test',
             disableAllCookies: false,
             disableThirdPartyCookies: false,
-            jobId: 2,
+            jobId: jobId,
             label: 'news',
-            maxPageCrawlDepth: 2,
+            maxPageCrawlDepth: options.max_page_crawl_depth,
             screenshotAdsWithContext: true,
             screenshotDir: './adscraper_screenshots',
             skipCrawlingSeedUrl: false,
             url: site,
-            warmingCrawl: false,
+            warmingCrawl: options.warm,
             updateCrawlerIpField: false
         }, postgres, thisProfile);
 
