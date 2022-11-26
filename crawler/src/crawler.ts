0f80ce4bfee05dbe15ba065388386cdae0e5c8ad
Sriram Viswanathan <sriram.vish@gmail.com>
2022-11-26 12:31:41 -0500

    Revert "Catch exception on scrolling and break"
    This reverts commit 3f25b51a01148b9fa90a8217e0b7c2da658eb1e4.

--- crawler/src/crawler.ts
+++ crawler/src/crawler.ts
@@ -223,12 +223,7 @@ async function crawlPage(page: puppeteer.Page, metadata: CrawlPageMetadata): Pro
 
       console.log("Beginning scroll");
       for (let counter: number = 0; counter < 60; counter++) {
-        try {
-          await scrollRandomly(page);
-        } catch (e) {
-          console.log(e)
-          break
-        }
+        await scrollRandomly(page);
       }
 
       const ads = await adDetection.identifyAdsInDOM(page);
