5829376e3c6f7f65eb1ccc92eae91440d6000fdd
Sriram Viswanathan <sriram.vish@gmail.com>
2022-11-26 12:51:28 -0500

    Open a new page before checking seed page

--- crawler/src/crawler.ts
+++ crawler/src/crawler.ts
@@ -513,6 +513,7 @@ export async function crawl(profileDirectory: string, extraPuppeteer: PuppeteerE
 
     // Open the seed page
     log.info(`${flags.url}: loading page`);
+    await browser.newPage()
     let seedPage = (await browser.pages())[0];
     if (!flags.skipCrawlingSeedUrl && !flags.warmingCrawl) {
       await domMonitor.injectDOMListener(seedPage);
